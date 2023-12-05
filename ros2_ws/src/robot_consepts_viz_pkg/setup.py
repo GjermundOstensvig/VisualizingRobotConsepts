9258e7d48c1411def383105079e31b3728f11be4
GjermumndOstensvig <gjermund1995@gmail.com>
2023-12-04 18:30:43 -0800

    Renaming launch file.

--- ros2_ws/src/robot_consepts_viz_pkg/setup.py
+++ ros2_ws/src/robot_consepts_viz_pkg/setup.py
@@ -1,32 +1,37 @@
 from glob import glob
 from setuptools import find_packages, setup
 
-package_name = 'robot_consepts_viz_pkg'
+package_name = "robot_consepts_viz_pkg"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        ('share/' +  package_name + '/meshes/stl', glob(pathname='meshes/**/**/*.stl', recursive=True)),
-        ('share/' +  package_name + '/meshes/dae', glob(pathname='meshes/**/**/*.dae', recursive=True)),
-        ('share/' + package_name + '/launch', ['launch/h_bot_display.launch.py']),
-        ('share/' + package_name + '/urdf', ['urdf/h_bot.urdf.xacro']),
-        ('share/' + package_name + '/rviz', ['rviz/my_rviz_config.rviz'])
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (
+            "share/" + package_name + "/meshes/stl",
+            glob(pathname="meshes/**/**/*.stl", recursive=True),
+        ),
+        (
+            "share/" + package_name + "/meshes/dae",
+            glob(pathname="meshes/**/**/*.dae", recursive=True),
+        ),
+        ("share/" + package_name + "/launch", ["launch/h_bot_gui.launch.py"]),
+        ("share/" + package_name + "/urdf", ["urdf/h_bot.urdf.xacro"]),
+        ("share/" + package_name + "/rviz", ["rviz/my_rviz_config.rviz"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='gjermund',
-    maintainer_email='gjermund1995@gmail.com',
-    description='TODO: Package description',
-    license='MIT',
-    tests_require=['pytest'],
+    maintainer="gjermund",
+    maintainer_email="gjermund1995@gmail.com",
+    description="TODO: Package description",
+    license="MIT",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-            'my_joint_state_publisher = robot_consepts_viz_pkg.my_joint_state_publisher:main'
+        "console_scripts": [
+            "my_joint_state_publisher = robot_consepts_viz_pkg.my_joint_state_publisher:main"
         ],
     },
 )
